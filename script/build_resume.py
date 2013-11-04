e8ca522d2d91b3e195270973ffd199eebf07404e
Ted Scharff <ted.scharff@gmail.com>
2013-11-03 17:14:55 -0800

    Added hobbies

--- script/build_resume.py
+++ script/build_resume.py
@@ -5,14 +5,23 @@ import yaml
 
 cwd = os.getcwd()
 jobs_data_filename = os.path.join(cwd, 'data/jobs.yaml')
+hobbies_filename = os.path.join(cwd, 'data/hobbies.yaml')
 outfile_name = 'resume/index.html'
-with open(jobs_data_filename, 'r') as jobsfile:
+
+with open(jobs_data_filename) as jobsfile:
     jobs = yaml.load(jobsfile.read())
+
+with open(hobbies_filename) as hobbiesfile:
+    hobbies = yaml.load(hobbiesfile.read())
 
 env = Environment(loader=FileSystemLoader(os.path.join(cwd,'templates/')))
 template = env.get_template("resume.html")
 
-output = template.render(jobs=jobs).encode('utf8')
+output = template.render(
+            jobs=jobs,
+            hobbies=hobbies,
+        ).encode('utf8')
+
 with open(outfile_name, 'w') as testfile:
     testfile.write(output)
 print "Wrote to %s" % outfile_name
